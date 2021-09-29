edd4a48a11c6f7645f946a132af5f2fb59b2d30b
Pandaroses <georgiytnimov@gmail.com>
2021-09-29 22:35:42 +0100

    added some cool stuff i guess

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,78 @@
 use std::io::stdout;
-use std::error::Error;
+use std::{error::Error, io};
 use tui::Terminal;
 use tui::backend::TermionBackend;
 use termion::raw::IntoRawMode;
 use termion::screen::AlternateScreen;
-use tui::widgets::{Widget, Block, Borders};
-use tui::layout::{Layout, Constraint, Direction};
+use termion::{event::Key, input::MouseTerminal};
+use tui::widgets::{Widget, Block, Borders, Paragraph, Wrap};
+use tui::layout::{Layout, Constraint, Direction, Alignment};
 use tui::text::{Span};
 use tui::style::{Style, Color};
+
+enum WindowMode {
+  Start,
+  Typing,
+}
+
+struct App {
+  input: String,
+  window_mode: WindowMode,
+  pastwpm: Vec<i32>,
+}
+
+impl Default for App {
+  fn default() -> App {
+    App {
+      input: String::new(),
+      window_mode: WindowMode::Start,
+      pastwpm: Vec::new(),
+    }
+  }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
   let backend = TermionBackend::new(AlternateScreen::from(stdout().into_raw_mode()?));
   let mut terminal = Terminal::new(backend)?;
+  let text: &str = "";
+  let mut app = App::default();
+
+  let help: &str = match app.window_mode {
+    WindowMode::Start =>("Welcome to Typecat, The Terminal Typing test =^._.^= ∫ \n q to quit ||  s to switch to typing mode || g to check previous typing speed"),
+    WindowMode::Typing=>("words")
+    
+  };
+
   loop {
     terminal.draw(|f| {
-      let chunk = Layout::default()
+      let chunks = Layout::default()
         .direction(Direction::Vertical)
-        .constraints([Constraint::Percentage(40), Constraint::Percentage(60)].as_ref())
+        .constraints([Constraint::Percentage(20), Constraint::Percentage(10)].as_ref())
         .split(f.size());
-      
-      f.render_widget(block, chunk[0]);
-      
+      let help = Paragraph::new(help)
+        .block(Block::default().borders(Borders::ALL))
+        .style(Style::default().fg(Color::White).bg(Color::DarkGray))
+        .alignment(Alignment::Center)
+        .wrap(Wrap { trim: true });
 
+      f.render_widget(help, chunks[0]);
+
+      let para = Paragraph::new(text)
+        .block(Block::default().borders(Borders::ALL))
+        .style(Style::default().fg(Color::White).bg(Color::DarkGray))
+        .alignment(Alignment::Center)
+        .wrap(Wrap { trim: true });
+
+      f.render_widget(para, chunks[1]);
+
+      let para2 = Paragraph::new(text)
+      .block(Block::default().borders(Borders::ALL))
+      .style(Style::default().fg(Color::Red).bg(Color::DarkGray))
+      .alignment(Alignment::Left)
+      .wrap(Wrap { trim: true });
       
     })?;
+    
   }
 
   Ok(())
